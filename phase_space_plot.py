2178262b8a5b775e6c1abc15d1349476ad59fd67
Andrea Jensen Marthinussen <andreajensenmarthinussen@eduroam-193-157-237-21.wlan.uio.no>
2022-06-27 16:22:36 +0200

    Continuation of phase space plot

--- phase_space_plot.py
+++ phase_space_plot.py
@@ -1,46 +1,64 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
-from sympy import diff, symbols, lambdify
-
-input1 = input('Please enter m q_0 p_0:')
-m = float(input1.split()[0]); q_0 = float(input1.split()[1]); p_0 = float(input1.split()[2])
-
-"""
-input2 = raw_input('Specify potential V:') '0.5 * m * omega ** 2 * x ** 2'
-for factor in input2.split():
-    try float(factor):
-        f
-
-    except ValueError:
-        if factor == '*':
-"""
+from sympy import symbols, diff, lambdify
 
 
-time_span = [0, 1000]
-init = [q_0, p_0]
+# Function that reads file containing initial values (& mass)
+def read_file(filename):
+    infile = open(filename, 'r')
+    lines = infile.readlines()
+    length = len(lines) - 1
+    m = float(lines[0])
+    init = np.zeros((2, len(lines)-1))
+
+    for i, line in enumerate(lines[1:]):
+        init[:,i] = np.array([float(line.split()[0]), float(line.split()[1])])
+
+    infile.close()
+    return init, length, m
 
 
-def V(q):
-    omega = 1
-    return 0.5*m*omega**2*q**2
+# Function containing the Hamiltonian as function of p's & q's
+def Hamiltonian(q, p):
+    omega = 1 # Parameters that must be specified
+    return p**2/(2*m) + 0.5*m*omega**2*q**2
 
-def integrand(t, qp):
-    q, p = symbols('q p')
-    H = p**2/(2*m) + V(q)
-    q_dot, p_dot = np.array([diff(H, p), -diff(H, q)])
 
+# Function determining Hamilton's equations by differentiating H
+def Hamiltons_Equations(t, qp):
+    q, p = symbols('q p') # Declaring q & p as sympy symbols
+
+    H = Hamiltonian(q, p)
+    q_dot, p_dot = np.array([diff(H, p), -diff(H, q)]) # Hamilton's equations
+
+    # Converting q_dot & p_dot from sympy expressions to numpy expressions
     q_dot = lambdify(p, q_dot, 'numpy')
     p_dot = lambdify(q, p_dot, 'numpy')
 
-    return np.array([q_dot(qp[1]), p_dot(qp[0])])
+    return np.array([q_dot(qp[1]), p_dot(qp[0])]) #
 
 
+# Reading input file: Recovering initial values (IVs), number of IVs and mass parameter
+init, length, m = read_file('input.txt')
+
+# Defining time interval
+time_span = [0, 1000]
 t = np.linspace(0,15,1000)
-sol = solve_ivp(integrand, time_span, y0 = init, t_eval = t)
 
 
-plt.plot(t, sol.y[0], label='q')
-plt.plot(t, sol.y[1], label='p')
-plt.legend()
+# Solving Hamilton's equations for each set of IVs (and storing them)
+solution = np.zeros((length, 2, len(t)))
+for i in range(length):
+    sol = solve_ivp(Hamiltons_Equations, time_span, y0 = init[:,i], t_eval = t)
+    solution[i, 0] = sol.y[0]
+    solution[i, 1] = sol.y[1]
+
+    # Plotting solutions q & p for each set of IVs
+    plt.plot(sol.y[0], sol.y[1], label=rf'$q_0 =$ {init[0,i]}, $p_0 =$ {init[1,i]}')
+    #plt.arrow(sol.y[0, 0], sol.y[1, 0], sol.y[0, 1], sol.y[1, 1]-sol.y[1, 0], shape='full', lw=0, length_includes_head=True, head_width=.05)
+    plt.legend()
+
+plt.xlabel(r'$q(t)$ [m]')
+plt.ylabel(r'$p(t)$ [kg$\cdot$m/s]')
 plt.show()
