7f2b4deb96773737524f06375d7017b22a25f98b
Andrea Jensen Marthinussen <andreajensenmarthinussen@eduroam-193-157-185-192.wlan.uio.no>
2022-06-28 17:32:36 +0200

    continuing on phase plot

--- ham_phase_space_plot.py
+++ ham_phase_space_plot.py
@@ -3,27 +3,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sympy import symbols, diff, lambdify, cos
-import be150
-import bokeh.io
 
 
 # Defining the Hamiltonian of the system
-def Hamiltonian(theta, p, arg):
+def Hamiltonian(q, p, arg):
     m = 1; g = 10; l = 1 # mass, gravitational acceleration & length of pendulum
     if arg == 'sympy':
-        return p**2/(2*m*l**2) + m*g*l*(1-cos(theta))
+        return p**2/(2*m*l**2) + m*g*l*(1-cos(q))
     elif arg == 'numpy':
-        return p**2/(2*m*l**2) + m*g*l*(1-np.cos(theta))
-
-
-# Defining linspaces for theta coordinate values & conjugate momentum values
-theta = np.linspace(np.pi, 0, 101)
-p = np.linspace(0, 10, 101)
-
-
-# Creating a meshgrid of the theta & conjugate momentum values
-X, Y = np.meshgrid(theta, p)
-Z = Hamiltonian(X, Y, 'numpy')
+        return p**2/(2*m*l**2) + m*g*l*(1-np.cos(q))
 
 
 # Function determining Hamilton's equations by differentiating H
@@ -37,32 +25,41 @@ def Hamiltons_Equations(qp):
     q_dot = lambdify(p, q_dot, 'numpy')
     p_dot = lambdify(q, p_dot, 'numpy')
 
-    return np.array([q_dot(qp[1]), p_dot(qp[0])]) #
+    return np.array([q_dot(qp[1]), p_dot(qp[0])])
 
 
-
-def flow_field():
-
-    # Compute derivatives
+# Making a stream plot of the Hamiltonian
+def Flow_Field():
+    # Computing derivatives
     u = np.empty_like(X)
     v = np.empty_like(Y)
+
     for i in range(X.shape[0]):
         for j in range(Y.shape[1]):
             u[i,j], v[i,j] = Hamiltons_Equations(np.array([X[i,j], Y[i,j]]))
 
-    # Make stream plot
-    return plt.streamplot(theta, p, u, v)
+    # Creating the plot
+    fig, ax = plt.subplots()
+    strm = ax.streamplot(X, Y, u, v, color=Z, linewidth=1, cmap='viridis')
+    fig.colorbar(strm.lines, label=r'$H(\theta, p_{\theta})$')
+    plt.xlabel(r'$\theta(t)$', fontsize=15)
+    plt.ylabel(r'$p_{\theta}(t)$', fontsize=15)
+    plt.title(r'Simple 1D pendulum', fontsize=20)
+    plt.tight_layout()
+    fig.savefig(f'streamplot.pdf')
+    plt.close()
+
+    return None
 
 
+# Defining linspaces for theta coordinate values & conjugate momentum values
+q = np.linspace(np.pi/2, 0, 101)
+p = np.linspace(0, 10, 101)
 
 
-plot = flow_field()
-#bokeh.io.show(plot)
+# Creating a meshgrid of the theta & conjugate momentum values
+X, Y = np.meshgrid(q, p)
+Z = Hamiltonian(X, Y, 'numpy')
 
-# Producing the contour plot of the Hamiltonian
-plt.contour(X, Y, Z, cmap='cividis', levels=80) #, shading='gouraud')  # gouraud
-plt.colorbar(label=r'$H(\theta, p_{\theta})$')
-plt.xlabel(r'$\theta(t)$', fontsize=15)
-plt.ylabel(r'$p_{\theta}(t)$', fontsize=15)
-plt.title(r'Simple 1D pendulum', fontsize=20)
-plt.show()
+
+Flow_Field()
--- phase_space_plot.py
+++ phase_space_plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
-from sympy import symbols, diff, lambdify
+#from sympy import symbols, diff, lambdify
 
 
 # Function that reads file containing initial values (& mass)
@@ -18,7 +18,7 @@ def read_file(filename):
     infile.close()
     return init, length, m
 
-
+"""
 # Function containing the Hamiltonian as function of p's & q's
 def Hamiltonian(q, p):
     omega = 1 # Parameters that must be specified
@@ -36,29 +36,44 @@ def Hamiltons_Equations(t, qp):
     q_dot = lambdify(p, q_dot, 'numpy')
     p_dot = lambdify(q, p_dot, 'numpy')
 
-    return np.array([q_dot(qp[1]), p_dot(qp[0])]) #
+    return np.array([q_dot(qp[1]), p_dot(qp[0])])
+"""
+
+# Function determining Hamilton's equations by differentiating H
+def Equations_of_Motion(t, alphabeta):
+    b = 1; l = 1; g = 10; # Parameters that must be specified
+
+    alpha, beta = alphabeta
+    alpha_dot = b*l/m*alpha - g*np.sin(beta)
+    beta_dot = alpha
+
+    return np.array([alpha_dot, beta_dot])
+
 
 
 # Reading input file: Recovering initial values (IVs), number of IVs and mass parameter
 init, length, m = read_file('input.txt')
-
+#print(init[:,0])
+#print(Equations_of_Motion(1, init[:,0]))
 # Defining time interval
-time_span = [0, 1000]
-t = np.linspace(0,15,1000)
+time_span = [0, 15]
+t = np.linspace(0,15,100)
 
 
 # Solving Hamilton's equations for each set of IVs (and storing them)
 solution = np.zeros((length, 2, len(t)))
 for i in range(length):
-    sol = solve_ivp(Hamiltons_Equations, time_span, y0 = init[:,i], t_eval = t)
+    sol = solve_ivp(Equations_of_Motion, time_span, y0 = init[:,i], t_eval = t)
     solution[i, 0] = sol.y[0]
     solution[i, 1] = sol.y[1]
+    print(sol.y[0], sol.y[1])
 
     # Plotting solutions q & p for each set of IVs
-    plt.plot(sol.y[0], sol.y[1], label=rf'$q_0 =$ {init[0,i]}, $p_0 =$ {init[1,i]}')
+    plt.plot(sol.y[1], sol.y[0], label=rf'$\alpha_0 =$ {init[0,i]}, $\beta_0 =$ {init[1,i]}')
+    #plt.plot(sol.t, sol.y[1])
     #plt.arrow(sol.y[0, 0], sol.y[1, 0], sol.y[0, 1], sol.y[1, 1]-sol.y[1, 0], shape='full', lw=0, length_includes_head=True, head_width=.05)
     plt.legend()
 
-plt.xlabel(r'$q(t)$ [m]')
-plt.ylabel(r'$p(t)$ [kg$\cdot$m/s]')
+plt.ylabel(r'$\dot{\theta}(t)$')
+plt.xlabel(r'$\theta(t)$')
 plt.show()
