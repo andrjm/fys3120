5c3457b075d6519587ed621110d8e2004cf1d86c
Andrea Jensen Marthinussen <andreajensenmarthinussen@eduroam-193-157-174-110.wlan.uio.no>
2022-07-06 17:08:11 +0200

    cleaning up

--- phase_space_plotting/phase_space_plot.py
+++ phase_space_plotting/phase_space_plot.py
@@ -1,7 +1,3 @@
-'''
-Put stuff here that shows what the thing does
-'''
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
@@ -21,7 +17,7 @@ def read_file(filename):
     infile.close()
     return init, length
 
-# This is too abstract for Hamltions equations, analytical mechanics symbols shpuld be used (q,p), the symbols must not become a barrier to understanding
+
 # Determining Hamilton's equations
 def Equations_of_Motion(t, alphabeta):
     m = 1; b = 0.2; l = 1; g = 10; # Parameters that must be specified
@@ -47,12 +43,12 @@ for i in range(length):
     sol = solve_ivp(Equations_of_Motion, time_span, y0 = init[:,i], t_eval = t)
 
     # Plotting solutions q & p for each set of IVs
-    plt.plot(sol.y[1], sol.y[0], label=rf'$\theta_0 =$ {init[1,i]}, '+
-    r'$\dot{\theta}_0 =$'+f' {init[0,i]}')
+    plt.plot(sol.y[1], sol.y[0], label=rf'$q_0 =$ {init[1,i]}, '+
+    r'$\dot{q}_0 =$'+f' {init[0,i]}')
     plt.legend()
 
-plt.ylabel(r'$\dot{\theta}(t)$', fontsize=15)
-plt.xlabel(r'$\theta(t)$', fontsize=15)
-plt.title(r'1D pendulum with damping', fontsize=20)
+plt.ylabel(r'$\dot{q}(t)$', fontsize=10)
+plt.xlabel(r'$q(t)$', fontsize=10)
+plt.title(r'1D pendulum with air resistance', fontsize=15)
 plt.savefig(f'non_ham_phasespace.pdf')
 plt.close()
