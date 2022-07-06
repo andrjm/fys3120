d3074c605fc576ffb135612413b7a55347ab79e7
Andrea Jensen Marthinussen <andreajensenmarthinussen@eduroam-193-157-174-110.wlan.uio.no>
2022-07-06 16:55:23 +0200

    cleaning up

--- phase_space_plotting/ham_phase_space_plot.py
+++ phase_space_plotting/ham_phase_space_plot.py
@@ -1,6 +1,3 @@
-'''
-Put stuff here that shows what the thing does
-'''
 # Simple 1D pendulum example
 
 import numpy as np
@@ -10,33 +7,33 @@ from sympy import symbols, diff, lambdify, cos
 
 # Defining the Hamiltonian of the system
 def Hamiltonian(q, p, arg):
-    m = 1; g = 10; l = 1 # mass, gravitational acceleration & length of pendulum
+    m = 1; g = 10; l = 1 # Mass, gravitational acceleration & length of pendulum
     if arg == 'sympy':
         return p**2/(2*m*l**2) + m*g*l*(1-cos(q))
     elif arg == 'numpy':
         return p**2/(2*m*l**2) + m*g*l*(1-np.cos(q))
 
 
-# Determining Hamilton's equations by differentiating H using sympy
+# Determining Hamilton's equations by differentiating H using SymPy
 def Hamiltons_Equations(qp):
-    q, p = symbols('q p') # Declaring q & p as sympy symbols
+    q, p = symbols('q p') # Declaring q & p as SymPy symbols
 
     H = Hamiltonian(q, p, 'sympy')
     q_dot, p_dot = np.array([diff(H, p), -diff(H, q)]) # Hamilton's equations
 
-    # Converting q_dot & p_dot from sympy expressions to numpy functions
+    # Converting q_dot & p_dot from SymPy expressions to NumPy functions
     q_dot = lambdify(p, q_dot, 'numpy')
     p_dot = lambdify(q, p_dot, 'numpy')
 
     return np.array([q_dot(qp[1]), p_dot(qp[0])])
 
 
-# Defining linspaces for theta coordinate values & conjugate momentum values
+# Defining linspaces for coordinate values & conjugate momentum values
 q = np.linspace(-2*np.pi, 2*np.pi, 101)
 p = np.linspace(-10, 10, 101)
 
 
-# Creating a meshgrid of the theta & conjugate momentum values
+# Creating a meshgrid of the coordinate & conjugate momentum values
 X, Y = np.meshgrid(q, p)
 Z = Hamiltonian(X, Y, 'numpy')
 
@@ -49,10 +46,10 @@ u, v = Hamiltons_Equations(np.array([X, Y]))
 # Making a stream plot of the Hamiltonian
 fig, ax = plt.subplots()
 strm = ax.streamplot(X, Y, u, v, color=Z, linewidth=1, cmap='viridis')
-fig.colorbar(strm.lines, label=r'$H(\theta, p_{\theta})$')
-plt.xlabel(r'$\theta(t)$', fontsize=15)
-plt.ylabel(r'$p_{\theta}(t)$', fontsize=15)
-plt.title(r'Simple 1D pendulum', fontsize=20)
+fig.colorbar(strm.lines, label=r'$H(q, p)$')
+plt.xlabel(r'$q(t)$', fontsize=10)
+plt.ylabel(r'$p(t)$', fontsize=10)
+plt.title(r'Simple 1D pendulum', fontsize=15)
 plt.tight_layout()
 fig.savefig(f'ham_phasespace.pdf')
 plt.close()
